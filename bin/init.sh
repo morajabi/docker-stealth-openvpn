7bc95ee519fddec60dd429ba8b0ae2c5579c1463
Mohamad <mo.rajbi@gmail.com>
2023-04-15 14:45:51 +0330

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -16,8 +16,8 @@ printf "This server's public address [%s]: " "$PUBLIC_IP"
 #read -r ADDRESS
 ADDRESS="${PUBLIC_IP}"
 
-docker-compose run --rm openvpn ovpn_genconfig  -C "AES-256-CBC" -a "SHA384" -u "tcp://$ADDRESS:443"
-docker-compose run --rm openvpn ovpn_initpki nopass
+docker compose run --rm openvpn ovpn_genconfig  -C "AES-256-CBC" -a "SHA384" -u "tcp://$ADDRESS:443"
+docker compose run --rm openvpn ovpn_initpki nopass
 
 cat >> "$OVPN_CONF_PATH" <<EOF
 push "verb 3"
